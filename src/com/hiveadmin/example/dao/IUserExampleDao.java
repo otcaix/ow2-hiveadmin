e2843e3e54cf89c72e240a03802dae3784c00c47
wangjie <wangjie12@otcaix.iscas.ac.cn>
2013-09-01 10:46:32 +0800

    1.remove useless code 2.refactor the structure

--- src/com/hiveadmin/example/dao/IUserExampleDao.java
+++
@@ -1,9 +0,0 @@
-package com.hiveadmin.example.dao;
-
-import com.hiveadmin.example.beans.UserExample;
-
-public interface IUserExampleDao {
-	public UserExample getUser(Integer id);
-	public void addUser(UserExample user);
-
-}
--- src/com/hiveadmin/example/service/ExampleUserService.java
+++
@@ -1,9 +0,0 @@
-package com.hiveadmin.example.service;
-
-import com.hiveadmin.example.beans.UserExample;
-
-public interface ExampleUserService {
-	public void login(UserExample user);
-	public void register(UserExample user);
-
-}
